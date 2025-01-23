5ad53065e5996dd196f0c624114c912e6389e057
sankalp <badonisankalp123@gmail.com>
2025-01-23 21:55:51 +0530

    made basic routes

---
+++ controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const register = (req, res)=>{
+    console.log("register endpoint");
+    
+}
+
+export const login = (req, res)=>{
+    
+}
+
+export const logout = (req, res)=>{
+    
+}
--- routes/post.route.js
+++ routes/post.route.js
@@ -6,4 +6,17 @@ router.get("/test",(req, res)=>{
     console.log("router works");
 });
 
+router.post("/test",(req, res)=>{
+    console.log("router works");
+});
+
+router.put("/test",(req, res)=>{
+    console.log("router works");
+});
+
+router.delete("/test",(req, res)=>{
+    console.log("router works");
+});
+
+
 export default router;
