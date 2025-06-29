f283f4596ec6a61a0cdb4efbccd4573b5c838360
sankalp <badonisankalp123@gmail.com>
2025-06-29 16:25:51 +0530

    updated app.js file

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ const app = express()
 app.use(express.json())  
 app.use(cookieParser());
 app.use(cors({
-    origin: 'https://urban-edge.vercel.app',
+    origin: [
+        'http://localhost:5173/',
+        'https://urban-edge.vercel.app'
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
