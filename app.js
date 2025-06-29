6119cdbe45db0b36ed39b4e016f8ba548e484fa9
sankalp <badonisankalp123@gmail.com>
2025-06-29 15:39:28 +0530

    updated cors

--- app.js
+++ app.js
@@ -14,10 +14,12 @@ const app = express()
 app.use(express.json())  
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173', 
-    credentials: true
+    origin: 'https://urban-edge.vercel.app',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['set-cookie'],
 }));
-
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("Connected to MongoDB"))
